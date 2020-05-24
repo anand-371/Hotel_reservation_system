de1e3534e6b6bc83a5e9c2e63d476fd6cda5f8df
Anand <40825655+anand-371@users.noreply.github.com>
2020-05-25 02:41:41 +0530

    added triggers

--- Query.sql
+++ Query.sql
@@ -10,12 +10,11 @@ CREATE TABLE Customer
   lastname VARCHAR(10) NOT NULL,
   middlename VARCHAR(10) NOT NULL,
   Age INT NOT NULL,
-  primary_contact_number CHAR(10) NOT NULL,
+  primary_contact_number CHAR(12) NOT NULL,
   country VARCHAR(20) NOT NULL,
   PRIMARY KEY (CustomerID),
   UNIQUE (SSN)
 );
-
 CREATE TABLE Hotel
 (
   HotelID INT NOT NULL,
@@ -34,7 +33,7 @@ CREATE TABLE payment_details
 );
 CREATE TABLE Hotel_Hotel_contact_number
 (
-  Hotel_contact_number INT NOT NULL,
+  Hotel_contact_number VARCHAR(12) NOT NULL,
   HotelID INT NOT NULL,
   PRIMARY KEY (Hotel_contact_number, HotelID),
   FOREIGN KEY (HotelID) REFERENCES Hotel(HotelID) ON DELETE CASCADE
@@ -71,26 +70,44 @@ CREATE TABLE Rooms
   HotelID INT NOT NULL,
   PRIMARY KEY (RoomID),
   FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID) ON DELETE CASCADE,
-  FOREIGN KEY (HotelID) REFERENCES Hotel(HotelID) ON DELETE CASCADE
+  FOREIGN KEY (HotelID) REFERENCES Hotel(HotelID) ON DELETE CASCADE,
+  FOREIGN KEY (reservationID) REFERENCES payment_details(reservationID) ON DELETE CASCADE
+);
+
+
+CREATE TABLE Income_details
+(
+ BillID INT NOT NULL,
+ amount_paid INT  
 );
 
 alter table Customer add constraint customer_valid check (
 email like '%_@__%.__%' AND
-Age>=18 AND 
-primary_contact_number like '[0-9]*10' AND
-postalcode like '[0-9]*6' );
+Age>=18 
+);
 
+
+/*alter table Customer DROP CONSTRAINT customer_valid;
+alter table Customer DROP CONSTRAINT  customer_valid ;
+*/
 alter table Rooms add constraint chk_room check (
 end_date>=start_date AND
 room_category IN ('Single','Double','Triple','Quad','King','Connecting','Suite') AND
 number_of_beds<=4
 );
+
 alter table Hotel_Hotel_contact_number add constraint chk_hotel_contact check (
-Hotel_contact_number like '[0-9]*10'
+Hotel_contact_number like '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'
 );
+/*
+alter table Hotel_Hotel_contact_number DROP CONSTRAINT chk_hotel_contact;
 alter table Hotel add constraint chk_hotel check (
 postalcode like '[0-9]*6'
+alter table Hotel DROP CONSTRAINT chk_hotel;
 );
+*/
+
+
 alter table Bill add constraint chk_bill check(
 Amount>0 
 );
@@ -100,11 +117,52 @@ payment_method IN ('Cash','Card','Netbanking','UPI') AND
 payment_status IN ('Failed','successful')
 );
 
+CREATE TRIGGER income_trigger ON Bill
+FOR INSERT AS
+BEGIN
+
+    INSERT INTO 
+    Income_details
+    (
+        BillID,
+        amount_paid
+    )
+    SELECT 
+        BillID,
+        Amount
+    FROM 
+        INSERTED
+
+END
 
 
+INSERT INTO Customer VALUES (12345,8999,'anand@gmail.com','bangalore','blahblah',560062,'meh','huh','uff',22,'6362134545','India');
+INSERT INTO Hotel VALUES (9999,'hehe',560061,'bangalore','abc');
+INSERT INTO payment_details VALUES (69696,'Cash','successful');
+INSERT INTO  Hotel_Hotel_contact_number VALUES ('8262134545',9999); 
+INSERT INTO  Bill(BillID,Amount,lastname,firstname,middlename,HotelID,reservationID,CustomerID) VALUES (1111,32,'meh','huh','pfft',9999,69696,8999);
+INSERT INTO Rooms VALUES (1093,'king',2,'2008-11-11','2008-12-11',10,69696,978,8999,9999);
+
+SELECT SUM(amount_paid)
+FROM Income_details
+WHERE amount_paid>0;
+
+Select r.ReservationID,r.CustomerID,b.BillID
+from Rooms as r
+JOIN Bill b on r.CustomerID=b.CustomerID
+
+SELECT * from Customer;
+SELECT * from Hotel;
+SELECT * from payment_details;
+SELECT * from Hotel_Hotel_contact_number;
+SELECT * from Bill;
+SELECT * from Rooms;
+SELECT * from Income_details;
+/*
 DROP TABLE Hotel_Hotel_contact_number;
 DROP TABLE Hotel;
 DROP TABLE Customer;
 DROP TABLE payment_details;
 DROP TABLE Rooms;
 DROP TABLE Bill;
+*/
