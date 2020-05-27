f8b45b9bd267d778ee3bd4428ff86c72f33c5ec9
Anand <40825655+anand-371@users.noreply.github.com>
2020-05-28 03:43:20 +0530

    changes in trigger

--- Query.sql
+++ Query.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Customer
-(
+( index_1 INT NOT NULL,
   SSN INT NOT NULL,
   CustomerID INT NOT NULL,
   email VARCHAR(20) NOT NULL,
@@ -16,7 +16,7 @@ CREATE TABLE Customer
   UNIQUE (SSN)
 );
 CREATE TABLE Hotel
-(
+( index_1 INT NOT NULL,
   HotelID INT NOT NULL,
   street VARCHAR(20),
   postalcode INT NOT NULL,
@@ -25,21 +25,21 @@ CREATE TABLE Hotel
   PRIMARY KEY (HotelID)
 );
 CREATE TABLE payment_details
-(
+( index_1 INT NOT NULL,
   reservationID INT NOT NULL,
   payment_method VARCHAR(10) NOT NULL,
   payment_status VARCHAR(10) NOT NULL,
   PRIMARY KEY (reservationID)
 );
 CREATE TABLE Hotel_Hotel_contact_number
-(
+( index_1 INT NOT NULL,
   Hotel_contact_number VARCHAR(12) NOT NULL,
   HotelID INT NOT NULL,
   PRIMARY KEY (Hotel_contact_number, HotelID),
   FOREIGN KEY (HotelID) REFERENCES Hotel(HotelID) ON DELETE CASCADE
 );
 CREATE TABLE Rooms
-(
+( index_1 INT NOT NULL,
   RoomID INT NOT NULL,
   room_category VARCHAR(10) NOT NULL,
   number_of_beds INT NOT NULL,
@@ -56,7 +56,7 @@ CREATE TABLE Rooms
   FOREIGN KEY (reservationID) REFERENCES payment_details(reservationID) ON DELETE CASCADE
 );
 CREATE TABLE Bill
-(
+( index_1 INT NOT NULL,
   BillID INT NOT NULL,
   Amount INT NOT NULL,
   lastname VARCHAR(10) NOT NULL,
@@ -73,7 +73,7 @@ CREATE TABLE Bill
 );
 
 CREATE TABLE AUDIT_DETAILS
-(
+( 
  BillID INT,
  start_date date,
  end_date date,
@@ -117,28 +117,28 @@ payment_method IN ('Cash','Card','Netbanking','UPI') AND
 payment_status IN ('Failed','successful')
 );
 
+
 CREATE TRIGGER income_trigger ON Bill
 FOR INSERT AS
 BEGIN
-	DELETE FROM AUDIT_DETAILS;
-    INSERT INTO 
-    AUDIT_DETAILS
-    (
-        BillID,
-        total_amount_paid,
+
+	DECLARE @temp INT
+	SELECT 
+		@temp=COALESCE(max(Bill.index_1),0)
+	from Bill;
+		INSERT INTO 
+		AUDIT_DETAILS
+		(
+			BillID,
+			total_amount_paid,
 		start_date,
 		end_date
-    )
-    SELECT t1.BillID, t1.Amount, t2.start_date, t2.end_date
-	FROM (
-	   SELECT BillID, Amount,CustomerID,
-			  ROW_NUMBER() OVER (ORDER BY BillID) AS rn
-	   FROM Bill) AS t1
-	  JOIN  (
-	   SELECT start_date, end_date,CustomerID,
-			  ROW_NUMBER() OVER (ORDER BY start_date) AS rn
-	   FROM Rooms) AS t2
-	ON t1.rn = t2.rn;
+		)
+		SELECT Bill.BillID, Bill.Amount, Rooms.start_date, Rooms.end_date
+		FROM Bill
+		INNER JOIN Rooms ON Bill.CustomerID=Rooms.CustomerID WHERE Bill.index_1=COALESCE(@temp,0);
+
+	
 
 	DECLARE @sd date;
 	DECLARE @ed date;
@@ -153,32 +153,16 @@ BEGIN
 
 END
 
-INSERT INTO Customer VALUES (12345,8999,'anand@gmail.com','bangalore','blahblah',560062,'meh','huh','uff',22,'6362134545','India');
-INSERT INTO Hotel VALUES (9999,'hehe',560061,'bangalore','abc');
-INSERT INTO payment_details VALUES (69696,'Cash','successful');
-INSERT INTO  Hotel_Hotel_contact_number VALUES ('8262134545',9999); 
-INSERT INTO Rooms VALUES (1093,'king',2,'2008-11-11','2008-12-11',10,69696,978,8999,9999);
-INSERT INTO  Bill(BillID,Amount,lastname,firstname,middlename,HotelID,reservationID,CustomerID) VALUES (1111,32,'meh','huh','pfft',9999,69696,8999);
+INSERT INTO Customer VALUES (1,12345,8999,'anand@gmail.com','bangalore','blahblah',560062,'meh','huh','uff',22,'6362134545','India');
+INSERT INTO Hotel VALUES (1,9999,'hehe',560061,'bangalore','abc');
+INSERT INTO payment_details VALUES (1,69696,'Cash','successful');
+INSERT INTO  Hotel_Hotel_contact_number VALUES (1,'8262134545',9999); 
+INSERT INTO Rooms VALUES (1,1093,'king',2,'2008-11-11','2008-12-11',10,69696,978,8999,9999);
+INSERT INTO  Bill(index_1,BillID,Amount,lastname,firstname,middlename,HotelID,reservationID,CustomerID) VALUES (1,1111,32,'meh','huh','pfft',9999,69696,8999);
 
-INSERT INTO Customer VALUES (12344,8998,'asdf@gmail.com','bangalore','blahblah',560061,'meh','huh','uff',22,'6362134544','India');
-INSERT INTO Hotel VALUES (9998,'hehe',560060,'bangalore','abc');
-INSERT INTO payment_details VALUES (69695,'Cash','successful');
-INSERT INTO  Hotel_Hotel_contact_number VALUES ('8262134545',9998); 
-INSERT INTO Rooms VALUES (1092,'king',1,'2009-01-11','2009-12-11',9,69695,977,8998,9998);
-INSERT INTO  Bill(BillID,Amount,lastname,firstname,middlename,HotelID,reservationID,CustomerID) VALUES (1110,31,'meh','huh','pfft',9998,69696,8999);
-
-SELECT SUM(total_amount_paid)
-FROM AUDIT_DETAILS
-WHERE total_amount_paid>0;
-
-Select r.ReservationID,r.CustomerID,b.BillID
-from Rooms as r
-JOIN Bill b on r.CustomerID=b.CustomerID
-
-SELECT * from Customer;
-SELECT * from Hotel;
-SELECT * from payment_details;
-SELECT * from Hotel_Hotel_contact_number;
-SELECT * from Bill;
-SELECT * from Rooms;
-SELECT * from AUDIT_DETAILS;
+INSERT INTO Customer VALUES (2,12344,8998,'asdf@gmail.com','bangalore','blahblah',560061,'meh','huh','uff',22,'6362134544','India');
+INSERT INTO Hotel VALUES (2,9998,'hehe',560060,'bangalore','abc');
+INSERT INTO payment_details VALUES (2,69695,'Cash','successful');
+INSERT INTO  Hotel_Hotel_contact_number VALUES (2,'8262134545',9998); 
+INSERT INTO Rooms VALUES (2,1092,'king',1,'2009-01-11','2009-12-11',9,69695,977,8998,9998);
+INSERT INTO  Bill(index_1,BillID,Amount,lastname,firstname,middlename,HotelID,reservationID,CustomerID) VALUES (2,1110,31,'meh','huh','pfft',9998,69696,8998);
