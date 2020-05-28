cda6bf0ecfdef2994a19aa99cbbd5cc0356fa0c4
Anand <40825655+anand-371@users.noreply.github.com>
2020-05-29 00:57:25 +0530

    added check constraint for postalcode

--- Query.sql
+++ Query.sql
@@ -1,11 +1,11 @@
 CREATE TABLE Customer
-( index_1 INT NOT NULL,
+( 
   SSN INT NOT NULL,
   CustomerID INT NOT NULL,
   email VARCHAR(20) NOT NULL,
   city VARCHAR(20) NOT NULL,
   street VARCHAR(20),
-  postalcode INT NOT NULL,
+  postalcode VARCHAR(20),
   firstname VARCHAR(10) NOT NULL,
   lastname VARCHAR(10) NOT NULL,
   middlename VARCHAR(10) NOT NULL,
@@ -16,23 +16,23 @@ CREATE TABLE Customer
   UNIQUE (SSN)
 );
 CREATE TABLE Hotel
-( index_1 INT NOT NULL,
+( 
   HotelID INT NOT NULL,
   street VARCHAR(20),
-  postalcode INT NOT NULL,
+  postalcode VARCHAR(20),
   city VARCHAR(20) NOT NULL,
   hotelname VARCHAR(20) NOT NULL,
   PRIMARY KEY (HotelID)
 );
 CREATE TABLE payment_details
-( index_1 INT NOT NULL,
+( 
   reservationID INT NOT NULL,
   payment_method VARCHAR(10) NOT NULL,
   payment_status VARCHAR(10) NOT NULL,
   PRIMARY KEY (reservationID)
 );
 CREATE TABLE Hotel_Hotel_contact_number
-( index_1 INT NOT NULL,
+( 
   Hotel_contact_number VARCHAR(12) NOT NULL,
   HotelID INT NOT NULL,
   PRIMARY KEY (Hotel_contact_number, HotelID),
@@ -83,7 +83,8 @@ CREATE TABLE AUDIT_DETAILS
 
 alter table Customer add constraint customer_valid check (
 email like '%_@__%.__%' AND
-Age>=18 
+Age>=18  AND
+postalcode like '[0-9][0-9][0-9][0-9][0-9][0-9]'
 );
 
 
@@ -99,13 +100,10 @@ number_of_beds<=4
 alter table Hotel_Hotel_contact_number add constraint chk_hotel_contact check (
 Hotel_contact_number like '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'
 );
-/*
-alter table Hotel_Hotel_contact_number DROP CONSTRAINT chk_hotel_contact;
+
 alter table Hotel add constraint chk_hotel check (
-postalcode like '[0-9]*6'
-alter table Hotel DROP CONSTRAINT chk_hotel;
+postalcode like '[0-9][0-9][0-9][0-9][0-9][0-9]'
 );
-*/
 
 
 alter table Bill add constraint chk_bill check(
@@ -153,16 +151,38 @@ BEGIN
 
 END
 
-INSERT INTO Customer VALUES (1,12345,8999,'anand@gmail.com','bangalore','blahblah',560062,'meh','huh','uff',22,'6362134545','India');
-INSERT INTO Hotel VALUES (1,9999,'hehe',560061,'bangalore','abc');
-INSERT INTO payment_details VALUES (1,69696,'Cash','successful');
-INSERT INTO  Hotel_Hotel_contact_number VALUES (1,'8262134545',9999); 
-INSERT INTO Rooms VALUES (1,1093,'king',2,'2008-11-11','2008-12-11',10,69696,978,8999,9999);
-INSERT INTO  Bill(index_1,BillID,Amount,lastname,firstname,middlename,HotelID,reservationID,CustomerID) VALUES (1,1111,32,'meh','huh','pfft',9999,69696,8999);
+INSERT INTO Customer VALUES (12345,8999,'anand@gmail.com','bangalore','street2','560072','Anand','V','hegde',20,'6362134545','India');
+INSERT INTO Hotel VALUES (9999,'street2','560061','bangalore','ITC');
+INSERT INTO payment_details VALUES (69696,'Cash','successful');
+INSERT INTO  Hotel_Hotel_contact_number VALUES ('8262134545',9999);
+INSERT INTO Rooms VALUES (1,1093,'King',2,'2008-11-11','2008-11-15',5000,69696,110,8999,9999);
+INSERT INTO  Bill(index_1,BillID,Amount,firstname,middlename,lastname,HotelID,reservationID,CustomerID) VALUES (1,1111,5000,'Anand','V','hegde',9999,69696,8999);
 
-INSERT INTO Customer VALUES (2,12344,8998,'asdf@gmail.com','bangalore','blahblah',560061,'meh','huh','uff',22,'6362134544','India');
-INSERT INTO Hotel VALUES (2,9998,'hehe',560060,'bangalore','abc');
-INSERT INTO payment_details VALUES (2,69695,'Cash','successful');
-INSERT INTO  Hotel_Hotel_contact_number VALUES (2,'8262134545',9998); 
-INSERT INTO Rooms VALUES (2,1092,'king',1,'2009-01-11','2009-12-11',9,69695,977,8998,9998);
-INSERT INTO  Bill(index_1,BillID,Amount,lastname,firstname,middlename,HotelID,reservationID,CustomerID) VALUES (2,1110,31,'meh','huh','pfft',9998,69696,8998);
+INSERT INTO Customer VALUES (12344,8998,'abcd@gmail.com','bangalore','street3',560061,'John','G','K',22,'9740299869','India');
+INSERT INTO Hotel VALUES (9998,'street1','560060','bangalore','Leela');
+INSERT INTO payment_details VALUES (69695,'Cash','successful');
+INSERT INTO  Hotel_Hotel_contact_number VALUES ('8262134542',9998);
+INSERT INTO Rooms VALUES (2,1092,'king',1,'2009-12-11','2009-12-14',6500,69695,102,8998,9998);
+INSERT INTO  Bill(index_1,BillID,Amount,firstname,middlename,lastname,HotelID,reservationID,CustomerID) VALUES (2,1110,6500,'John','G','K',9998,69695,8998);
+
+INSERT INTO Customer VALUES (12343,8997,'bcde@gmail.com','bangalore','street3',560061,'Jack','K','M',30,'9748299869','India');
+INSERT INTO Hotel VALUES (9997,'street1','550043','chennai','Leela');
+INSERT INTO payment_details VALUES (69694,'Cash','successful');
+INSERT INTO  Hotel_Hotel_contact_number VALUES ('8262004542',9997);
+INSERT INTO Rooms VALUES (3,1091,'king',2,'2010-09-01','2010-09-05',7500,69695,102,8997,9997);
+INSERT INTO  Bill(index_1,BillID,Amount,firstname,middlename,lastname,HotelID,reservationID,CustomerID) VALUES (3,1109,7500,'Jack','K','M',9997,69694,8997);
+
+INSERT INTO Customer VALUES (12342,8996,'bcdfe@gmail.com','bangalore','street2',560161,'Max','L','K',25,'9782299869','India');
+INSERT INTO Hotel VALUES (9996,'street4','560001','bangalore','ITC');
+INSERT INTO payment_details VALUES (69693,'Card','successful');
+INSERT INTO  Hotel_Hotel_contact_number VALUES ('8262134545',9996);
+INSERT INTO Rooms VALUES (4,1094,'King',1,'2008-10-24','2008-10-26',8000,69693,110,8996,9996);
+INSERT INTO Bill(index_1,BillID,Amount,firstname,middlename,lastname,HotelID,reservationID,CustomerID) VALUES (4,1108,8000,'Max','L','K',9996,69693,8996);
+
+
+INSERT INTO Customer VALUES (12341,8995,'sjufe@gmail.com','bangalore','street1',563461,'Alex','K','T',60,'9088299869','India');
+INSERT INTO Hotel VALUES (9995,'street3','563001','bangalore','Leela');
+INSERT INTO payment_details VALUES (69692,'Card','successful');
+INSERT INTO  Hotel_Hotel_contact_number VALUES ('8362134545',9995);
+INSERT INTO Rooms VALUES (5,1095,'King',3,'2008-10-25','2008-10-29',12000,69692,201,8995,9995);
+INSERT INTO Bill(index_1,BillID,Amount,firstname,middlename,lastname,HotelID,reservationID,CustomerID) VALUES (5,1107,12000,'Alex','K','T',9995,69692,8995);
